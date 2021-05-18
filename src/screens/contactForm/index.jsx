0af8fd530ec5e188298c1815d22ad6323e194212
Jae Ung Kim <jaeung95@student.ubc.ca>
2021-05-18 08:47:14 -0700

    Update index.jsx
    asdf

--- src/screens/contactForm/index.jsx
+++ src/screens/contactForm/index.jsx
@@ -29,83 +29,85 @@ const ContactForm = () => {
           <h1> Contact Me</h1>
         </div>
         <div className="justify-content-center text-left">
-          <div className="row">
-            <form
-              onSubmit={submitForm}
-              action="https://formspree.io/xayprage"
-              method="POST"
-              className="pt-2 contact_form"
-            >
-              <div className="col-lg">
-                <div className="form-group mt-2">
-                  <label htmlFor="name" className="font-weight-bold">
-                    {" "}
-                    Name{" "}
-                  </label>
-                  <input
-                    name="name"
-                    id="name"
-                    type="text"
-                    className="form-control"
-                    placeholder="Your name..."
-                  />
+          <div className="container">
+            <div className="row">
+              <form
+                onSubmit={submitForm}
+                action="https://formspree.io/xayprage"
+                method="POST"
+                className="pt-2 contact_form"
+              >
+                <div className="col-lg">
+                  <div className="form-group mt-2">
+                    <label htmlFor="name" className="font-weight-bold">
+                      {" "}
+                      Name{" "}
+                    </label>
+                    <input
+                      name="name"
+                      id="name"
+                      type="text"
+                      className="form-control"
+                      placeholder="Your name..."
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div className="col-lg">
-                <div className="form-group mt-2">
-                  <label htmlFor="email" className="font-weight-bold">
-                    {" "}
-                    Email address{" "}
-                  </label>
+                <div className="col-lg">
+                  <div className="form-group mt-2">
+                    <label htmlFor="email" className="font-weight-bold">
+                      {" "}
+                      Email address{" "}
+                    </label>
+                    <input
+                      name="email"
+                      id="email"
+                      type="email"
+                      className="form-control"
+                      placeholder="Your email..."
+                    />
+                  </div>
+                </div>
+                <div className="col-lg">
+                  <div className="form-group mt-2">
+                    <label htmlFor="comments" className="font-weight-bold">
+                      {" "}
+                      Message{" "}
+                    </label>
+                    <textarea
+                      name="comments"
+                      id="comments"
+                      rows="4"
+                      className="form-control"
+                      placeholder="Your message..."
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="col-lg text-right">
+                  {status === "SUCCESS" ? (
+                    <p className="success_message">
+                      Your message has beent sent!
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                  {status === "FAIL" ? (
+                    <p className="error_message">
+                      Oops! Try again! Make sure you entered info correctly
+                    </p>
+                  ) : (
+                    ""
+                  )}
                   <input
-                    name="email"
-                    id="email"
-                    type="email"
-                    className="form-control"
-                    placeholder="Your email..."
-                  />
+                    type="submit"
+                    id="submit"
+                    name="send"
+                    className="btn btn-custom"
+                    value="Send Message"
+                  ></input>
                 </div>
-              </div>
-              <div className="col-lg">
-                <div className="form-group mt-2">
-                  <label htmlFor="comments" className="font-weight-bold">
-                    {" "}
-                    Message{" "}
-                  </label>
-                  <textarea
-                    name="comments"
-                    id="comments"
-                    rows="4"
-                    className="form-control"
-                    placeholder="Your message..."
-                  ></textarea>
-                </div>
-              </div>
-              <div className="col-lg text-right">
-                {status === "SUCCESS" ? (
-                  <p className="success_message">
-                    Your message has beent sent!
-                  </p>
-                ) : (
-                  ""
-                )}
-                {status === "FAIL" ? (
-                  <p className="error_message">
-                    Oops! Try again! Make sure you entered info correctly
-                  </p>
-                ) : (
-                  ""
-                )}
-                <input
-                  type="submit"
-                  id="submit"
-                  name="send"
-                  className="btn btn-custom"
-                  value="Send Message"
-                ></input>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </section>
