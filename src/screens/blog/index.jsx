2cc2abe35ebae990c3ea1f39733ffca74e4756dd
Jae Kim <jae.kim@catalyx.io>
2022-01-08 18:14:38 -0800

    cloud distribution error page fix

--- src/screens/blog/index.jsx
+++ src/screens/blog/index.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import "./local.scss";
 
 const Blog = () => {
-  return <div>Start of Blog</div>;
+  return <div>Start of Blog. Cloud Distribution fix</div>;
 };
 export default Blog;
