ceffd7987604c068df9c2da8000e0ae0a486a57e
Jae Kim <jae.kim@catalyx.io>
2022-02-04 13:07:44 -0800

    hm

--- src/screens/clickjacking/index.jsx
+++ src/screens/clickjacking/index.jsx
@@ -5,6 +5,15 @@ const Clickjacking = () => {
   return (
     <div>
       <p>Website is vulnerable to clickjacking!</p>
+
+          <iframe
+            src="https://www.youtube.com/embed/ZGejseN8P5s"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullscreen
+            title="youtube video player"
+
+          />
+  
       <iframe
         src="http://www.yoursite.com/sensitive-page"
         width="500"
