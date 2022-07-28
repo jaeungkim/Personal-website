21ad852ed446c96a64562e47e5390a8687a241a9
Jae Kim <43080200+jaeungkim@users.noreply.github.com>
2022-07-28 10:15:23 -0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HeaderComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
