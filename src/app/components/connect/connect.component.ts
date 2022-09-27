62fa42478a6d3e2b3b0dfd790b56323a99037cd6
jaeungkim <jaeung95@student.ubc.ca>
2022-09-27 15:39:45 -0700

    updates

--- src/app/components/connect/connect.component.ts
+++ src/app/components/connect/connect.component.ts
@@ -1,18 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-connect',
   templateUrl: './connect.component.html',
-  styleUrls: ['./connect.component.css']
+  styleUrls: ['./connect.component.css'],
 })
 export class ConnectComponent implements OnInit {
+  contactUsForm: FormGroup;
   btnclick: boolean = false;
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
 
-  btnclickEvent(){
+  ngOnInit(): void {
+    // console.log(this.contactUsForm.value.province)
+  }
+
+  createForm() {
+    this.contactUsForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      organization: ['', Validators.required],
+      position: ['', Validators.required],
+      email: ['', Validators.email],
+      phone: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]*$'),
+          Validators.minLength(10),
+          Validators.maxLength(10),
+        ],
+      ],
+      province: ['', Validators.required],
+    });
+  }
+  btnclickEvent() {
     this.btnclick = !this.btnclick;
+  }
+  onSubmit() {
+    console.log('');
   }
 }
