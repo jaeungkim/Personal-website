1e355edb3424209ab46d3b03d19d94805cf3aefb
Jae Kim <43080200+jaeungkim@users.noreply.github.com>
2022-11-01 12:21:00 -0700

    ok

--- src/app/components/connect/connect.component.ts
+++ src/app/components/connect/connect.component.ts
@@ -54,6 +54,7 @@ export class ConnectComponent implements OnInit {
       },
       (error) => {
         this.buttonerror = true;
+        this.contactUsForm.reset();
         console.log(error);
       }
     );
--- src/app/services/email/email.service.ts
+++ src/app/services/email/email.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class EmailService {
   constructor(private http: HttpClient) {}
-
-  sendEmail(input: any) {
-    return this.http
     // https://0ffpeiwy24.execute-api.us-east-1.amazonaws.com/default/ContactFormLambda //jaekim
     // https://12dwurj4sl.execute-api.us-east-1.amazonaws.com/default/contactFormLambda 
-      .post('https://0ffpeiwy24.execute-api.us-east-1.amazonaws.com/default/ContactFormLambda', input,{observe: 'response'})
+
+  sendEmail(data: Object): Observable<Object> {
+    return this.http.post('https://12dwurj4sl.execute-api.us-east-1.amazonaws.com/default/contactFormLambda', data)   
   }
 }
