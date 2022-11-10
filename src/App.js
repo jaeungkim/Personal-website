07aee159011c2fbbe0f24b16b965ea2152e5dd14
jaeungkim <jaeung95@student.ubc.ca>
2022-11-09 21:06:05 -0800

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import "./App.scss";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
-
+import "./App.scss";
 function App() {
   return (
     <div>
-      <Navbar />
-      <About />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
